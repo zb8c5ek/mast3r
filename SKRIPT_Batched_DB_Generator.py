973664f082fc51c7abafb97128e7848d40ec5985
Fan-Huo <chen_alphonse_xuanli@outlook.com>
2026-01-12 10:02:14 +0800

    added SKRIPT for batch produce

--- SKRIPT_Batched_DB_Generator.py
+++ SKRIPT_Batched_DB_Generator.py
@@ -521,14 +521,14 @@ if __name__ == "__main__":
     start_time = time()
     
     # Configuration
-    dp_images = Path("/d_disk/_DataBuffer/RopeCap/20251224_103638/parsed_data/undistort_fov110_720sq_final/cam0/p-20_y+15_r+0")
+    dp_images = Path("/d_disk/_DataBuffer/RopeCap/20251224_103638/parsed_data/undistort_fov110_720sq_final/cam0/p+0_y+15_r+0")
     dp_output = dp_images.parent / ("mapping3r_batched_%s" % dp_images.name)
     
     # Batch parameters
     BATCH_SIZE = 40  # Number of images per batch
     OVERLAP = 10     # Number of overlapping images between batches
     MAPPER = None  # Options: 'GLOMAP', 'COLMAP', or None to skip mapping
-    START_FRAME = 80  # Frame index to start from (0 = beginning, use to resume processing)
+    START_FRAME = 0  # Frame index to start from (0 = beginning, use to resume processing)
     SPACING = 1      # Frame spacing (1 = consecutive, 2 = every 2nd frame, etc.)
     
     # Load model
